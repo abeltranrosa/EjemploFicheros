0ce90f91d38902c13aef3f3e707d2a66a441dddd
antonio <antonio@biruseto>
2017-03-07 11:56:12 +0100

    clase leerLinea ejemplo Ficheros

---
+++ src/Lectura/LeerLinea.java
@@ -0,0 +1,27 @@
+package Lectura;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LeerLinea {
+
+    File fich;
+    Scanner sc;
+
+    public void Ler(String nombreFich) {
+        fich = new File(nombreFich);
+        try {
+            sc = new Scanner(fich);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+
+        } catch (FileNotFoundException ex) {
+            ex.getMessage();
+        } finally {
+            sc.close();
+        }
+    }
+
+}
